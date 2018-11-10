bf9b584c4ea4634fa24f7e43f1b28b22ffedd246
Dmitry Zabrodin <234518@mail.ru>
2018-11-10 18:20:11 +0300

    Shell created

---
+++ src/MainClass.java
@@ -0,0 +1,24 @@
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class MainClass
+{
+
+	public static void main(String[] args)
+	{
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		
+		shell.open();
+		
+		while (!shell.isDisposed())
+		{
+			if (!display.readAndDispatch())
+			display.sleep();
+		}
+		display.dispose();
+		
+	}
+	
+}
